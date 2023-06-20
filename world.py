4284f3373234dcdf4cb811ab6c615a6f4f5a5b47
John <j.grobben@hr.nl>
2023-06-20 16:41:42 +0200

    grass is growing now

--- world.py
+++ world.py
@@ -23,13 +23,19 @@ class World:
                     self.grid[row][col] = (tile_value, color)
 
     def update(self):
-        pass
+        for row in range(self.size):
+            for col in range(self.size):
+                tile_value, color = self.grid[row][col]
+                if tile_value in self.GRASS:
+                    new_tile_value = min(tile_value + 1, 10)
+                    new_color = self.get_color(tile_value)
+                    self.grid[row][col] = (new_tile_value, new_color)
 
     def draw_world(self, screen):
         for row in range(self.size):
             for col in range(self.size):
                 tile_value, color = self.grid[row][col]
-                pygame.draw.rect(screen, color, (col, row, 2,2)) # rect is not necessary
+                pygame.draw.rect(screen, color, (col, row, 1,1)) # rect is not necessary
 
     def give_information_about_location(self, row, col):
         tile_value, _ = self.grid[row][col]
