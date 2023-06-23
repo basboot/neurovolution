520c7dac0eaad9c82bfcaa0d11eaead464ae20fc
Bas Boot <github@basboot.nl>
2023-06-23 16:50:03 +0200

    Cleanup obsolete functions

--- brain.py
+++ brain.py
@@ -18,28 +18,8 @@ def create_random_brain(layers, nn_connections, seed=None):
 
         random_brain += [np.random.random() * 1.0 for _ in range(layer_output_size * layer_input_size + layer_output_size)]
 
-
-
     return random_brain
 
-
-def init_layers(nn_architecture, brain, seed=99):
-    params_values = []
-
-    brain_index = 0
-    for idx, layer in enumerate(nn_architecture):
-        layer_input_size = layer["input_dim"]
-        layer_output_size = layer["output_dim"]
-
-        W = np.reshape(brain[brain_index:brain_index + layer_output_size * layer_input_size], (layer_output_size, layer_input_size))
-        b = np.reshape(brain[brain_index + layer_output_size * layer_input_size:brain_index + layer_output_size * layer_input_size + layer_output_size], (layer_output_size, 1))
-
-        brain_index += layer_output_size * layer_input_size + layer_output_size
-        layer_params = [W, b]
-
-        params_values.append(layer_params)
-
-    return params_values
 
 def sigmoid(Z):
     return 1/(1+np.exp(-Z))
@@ -47,41 +27,6 @@ def sigmoid(Z):
 def relu(Z):
     return np.maximum(0,Z)
 
-def sigmoid_backward(dA, Z):
-    sig = sigmoid(Z)
-    return dA * sig * (1 - sig)
-
-def relu_backward(dA, Z):
-    dZ = np.array(dA, copy = True)
-    dZ[Z <= 0] = 0
-    return dZ
-
-
-def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
-    Z_curr = np.dot(W_curr, A_prev) + b_curr
-
-    if activation == "relu":
-        activation_func = relu
-    elif activation == "sigmoid":
-        activation_func = sigmoid
-    else:
-        raise Exception('Non-supported activation function')
-
-    return activation_func(Z_curr), Z_curr
-
-
-def full_forward_propagation(X, params_values, nn_architecture):
-    A_curr = X
-
-    for idx, layer in enumerate(nn_architecture):
-        A_prev = A_curr
-
-        activ_function_curr = layer["activation"]
-        W_curr = params_values[idx][0]
-        b_curr = params_values[idx][1]
-        A_curr, Z_curr = single_layer_forward_propagation(A_prev, W_curr, b_curr, activ_function_curr)
-
-    return A_curr
 
 class Brain:
     def __init__(self, config, brain):
